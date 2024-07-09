3ef198cea69b64461776cddcbfde318c85f90f52
home_pc <you@example.com>
2024-07-10 00:36:20 +0300

    finalize

--- 01/json2json.cpp
+++ 01/json2json.cpp
@@ -4,15 +4,15 @@
 // then print like-json format by pass through ptree
 // 
 // EXAMPLE:                                  | OUTPUT:
-// { "name": {                               |  [name]: [aa ]: 1
-//     "aa" : 1,   "d": "44",                |          [d  ]: 44
-//     "abc": { "1": "s", "dv": 3},          |          [abc]: [1 ]: s
-//       "x":                                |                 [dv]: 3
-//         { "my" : "name", "test": "lain",  |          [x  ]: [my  ]: name
-//           "o p": 0.01  , "exp" : 2.0   }  |                 [test]: lain
-// }, "age": "123" }                         |                 [o p ]: 0.01
-//                                           |                 [exp ]: 2.0
-//                                           |  [age ]: 123
+// { "name": {                               | [name]: [aa ]: 1
+//     "aa" : 1,     "d": "44",              |         [d  ]: 44
+//     "abc": { "1": "s", "dv": 3},          |         [abc]: [1 ]: s
+//       "x":                                |                [dv]: 3
+//         { "my" : "name", "test": "lain",  |         [x  ]: [my  ]: name
+//           "o p": 0.01  , "exp" : 2.0   }  |                [test]: lain
+// }, "age": "123" }                         |                [o p ]: 0.01
+//                                           |                [exp ]: 2.0
+//                                           | [age ]: 123
 
 
 #include <boost/property_tree/ptree.hpp>
@@ -27,7 +27,7 @@ T max_in_layer(const Container& Cont, T (*get_size)(const typename Container::va
     return result;
 }
 
-void print(std::ostream& out, const boost::property_tree::ptree& t, int shift = 0) {
+void print_ptree(std::ostream& out, const boost::property_tree::ptree& t, int shift = 0) {
     const std::string ender = "\n";
     bool no_first = 0;
 
@@ -38,26 +38,25 @@ void print(std::ostream& out, const boost::property_tree::ptree& t, int shift = 
 
         out << "[" << std::left << std::setw(width) << k << "]: ";
         
-        if( !v.empty() ) print(out, v, shift+width+4);  // + 4 символа на: "[]: "
-        else             out << v.get_value<std::string>();
+        if( !v.empty() ) print_ptree(out, v, shift+width+4);  // + 4 символа на: "[]: "
+        else             out << v.get_value<std::string>( );
     }
 }
 
 std::ostream& operator << (std::ostream& out, const boost::property_tree::ptree& t) {
-    if( t.empty() ) out << "Empty ptree";
-    else print(out, t);
+    if( t.empty() )  out << "Empty ptree";
+    else print_ptree(out, t);
     return out;
 }
 
 int main(int argc, char* argv[]) {
-    char *o = argv[1];
-    char *p = o;
-    std::string filename = (char*)((const char* []){"test.json", o})[0];
+    std::string filename = (const char* []){"test.json", argv[1]}[argc != 1];
 
     boost::property_tree::ptree pt;
-    boost::property_tree::read_json("t.json", pt);
+    try { boost::property_tree::read_json(filename, pt); }
+    catch(...) { std::cout << "Can't read JSON from \"" << filename << "\"\n"; return 1; }
 
-    std::cout << "EXAMPLE:\n";
+    std::cout << "File \"" << filename << "\":\n";
     std::cout << pt;
     std::cout << std::endl;
     
